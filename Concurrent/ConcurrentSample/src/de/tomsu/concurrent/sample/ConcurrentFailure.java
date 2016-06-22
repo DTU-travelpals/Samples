77549a57200e5628329fbb6962f71f2348f18959
Daniel Tomsu <daniel.tomsu@travelpals.de>
2016-06-22 23:40:36 +0200

    changed for concurrent classes

--- Concurrent/ConcurrentSample/src/de/tomsu/concurrent/sample/ConcurrentFailure.java
+++ Concurrent/ConcurrentSample/src/de/tomsu/concurrent/sample/ConcurrentFailure.java
@@ -1,15 +1,15 @@
 package de.tomsu.concurrent.sample;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ConcurrentFailure {
 
 	public static void main(String[] args) {
-		List<String> myList = new ArrayList<String>();
+		List<String> myList = new CopyOnWriteArrayList<String>();
 
 		myList.add("1");
 		myList.add("2");
@@ -17,15 +17,19 @@ public class ConcurrentFailure {
 		myList.add("4");
 		myList.add("5");
 
-//		Iterator<String> it = myList.iterator();
-//		while (it.hasNext()) {
-//			String value = it.next();
-//			System.out.println("List Value:" + value);
-//			if (value.equals("3"))
-//				myList.remove(value);
-//		}
+		Iterator<String> it = myList.iterator();
+		while (it.hasNext()) {
+			String value = it.next();
+			System.out.println("List Value:" + value);
+			if (value.equals("3")) {
+				myList.remove("4");
+				myList.add("6");
+				myList.add("7");
+			}
+		}
+		System.out.println("List Size:" + myList.size());
 
-		Map<String, String> myMap = new HashMap<String, String>();
+		Map<String, String> myMap = new ConcurrentHashMap<String, String>();
 		myMap.put("1", "1");
 		myMap.put("2", "2");
 		myMap.put("3", "3");
@@ -34,11 +38,14 @@ public class ConcurrentFailure {
 		while (it1.hasNext()) {
 			String key = it1.next();
 			System.out.println("Map Value:" + myMap.get(key));
-			if (key.equals("2")) {
-				myMap.put("1", "4");
-				// myMap.put("4", "4");
+			if (key.equals("1")) {
+				myMap.remove("3");
+				myMap.put("4", "4");
+				myMap.put("5", "5");
 			}
 		}
+
+		System.out.println("Map Size:" + myMap.size());
 	}
 
 }
